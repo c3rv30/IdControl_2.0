5d3e524c414a2fd31230baa0ab1603a32d7b9923
Bryan Bobadilla <bbobadilla_pisero@icloud.com>
2016-05-31 23:29:42 -0400

    Versión inicial

---
+++ src/com/blacklist/login/MainLogin.java
@@ -0,0 +1,14 @@
+package com.blacklist.login;
+
+
+import android.os.Bundle;
+import com.zkc.barcodescan.R;
+import android.app.Activity;
+
+public class MainLogin extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_login);
+    }
+}
